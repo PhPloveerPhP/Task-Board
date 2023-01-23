22e0ba8bf1dd875b6a9ba44fb61ba378022af620
Mario <marioperezmarrero@gmail.com>
2023-01-23 16:50:58 +0000

    delete controller finished

--- controllers/delete_task.php
+++ controllers/delete_task.php
@@ -10,12 +10,10 @@ $config = include './config/config.php';
 try {
     $dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'];
     $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-    
-      $id_task = array_keys($_POST["delete"])[0];
 
     $task = [
         "id_user" => 1,
-        "id_task" => $id_task,
+        "id_task" => array_keys($_POST["delete"])[0],
     ];
 
     $sentence = $conexion->prepare("DELETE FROM tasks WHERE id_task = :task and id_user = :user");

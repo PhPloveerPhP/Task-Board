b9e018783aa799f40d73f860820641cd5d12d1ba
Mario <marioperezmarrero@gmail.com>
2023-01-31 02:19:33 +0000

    completed pending deployment

--- config/config.php
+++ config/config.php
@@ -1,18 +1,20 @@
 <?php
 
-return [
-    'db' => [
-        'host' => 'localhost',
-        'user' => 'root',
-        'pass' => '',
-        'name' => 'taskboard',
-        'options' => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    if (!isset($_ENV['CLEARDB_DATABASE_URL'])) {
+        require __DIR__ . '/../vendor/autoload.php';
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+    }
+
+    return [
+        'db' => [
+            'host' => $_ENV['DB_HOST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'name' => $_ENV['DB_NAME'],
+            'options' => [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
         ]
-
     ]
-
-];
-
-
 ?>

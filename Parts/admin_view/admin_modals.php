b1c075babad2e01d2e4e873f90cb602998a9385d
Mario <marioperezmarrero@gmail.com>
2023-02-02 15:41:17 +0000

    done

--- Parts/admin_view/admin_modals.php
+++ Parts/admin_view/admin_modals.php
@@ -1,4 +1,4 @@
-<!-- Create User -->
+  <!-- Create User -->
 <div class="modal fade" id="create_usr" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
